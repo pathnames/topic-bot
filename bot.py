15fe61929fc9d227e34c502d1a5138ba95a65e3e
pjkpjk900 <pk58@buffalo.edu>
2020-04-29 16:11:23 -0400

    Added one topic file and bot suggests topics from it

--- bot.py
+++ bot.py
@@ -1,8 +1,16 @@
-import discord, os
+import discord, os, random
 from discord.ext import commands 
 
 api_key = os.environ.get('topic-bot')
 client = commands.Bot(command_prefix = '!')
+
+beauty_topics = []
+
+#Open and read file for topics related to beauty and add them to the list
+with open('topics/beauty.txt') as f:
+    for line in f:
+        beauty_topics.append(line)
+
 
 @client.event
 async def on_ready():
@@ -10,9 +18,9 @@ async def on_ready():
 
 @client.command()
 async def topic(ctx):
-    await ctx.send("Sending new topic!")
-
-
+    idx = random.randrange(0, len(beauty_topics))
+    topic_string = beauty_topics[idx]
+    await ctx.send(topic_string)
 
 client.run(api_key)
 
