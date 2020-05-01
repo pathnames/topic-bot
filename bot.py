e61e28b278489a4bff8fcc3247119090f6985d9f
pjkpjk900 <pk58@buffalo.edu>
2020-04-30 23:23:59 -0400

    Added conversation topics about animals and travel and cleaned up bot.py

--- bot.py
+++ bot.py
@@ -4,20 +4,9 @@ from discord.ext import commands
 api_key = os.environ.get('topic-bot')
 client = commands.Bot(command_prefix = '!')
 
-#Global variables for topics:
-# beauty_topics = []
-# morality_topics = []
+#Array to hold topics:
 topics = []
 
-# #Open and read file for topics related to beauty and add them to the list
-# with open('topics/beauty.txt') as f:
-#     for line in f:
-#         beauty_topics.append(line)
-
-# #Open and read file for topics related to morality and add them to the list
-# with open('topics/morality.txt') as f:
-#     for line in f:
-#         morality_topics.append(line)
 
 #Open and write each line of each file in the topics directory:
 for path in pathlib.Path('topics').iterdir():
@@ -28,12 +17,6 @@ for path in pathlib.Path('topics').iterdir():
 @client.event
 async def on_ready():
     print('Bot is online!')
-
-# @client.command()
-# async def btopic(ctx):
-#     idx = random.randrange(0, len(beauty_topics))
-#     topic_string = beauty_topics[idx]
-#     await ctx.send(topic_string)
 
 @client.command()
 async def topic(ctx):
